a50ad1290ad512790b6cacdbb9cb19de646b7030
Samuel Morello <samuel.morello@ouvanous.com>
2013-12-10 10:39:16 +0100

    2013-12-10 10:39

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-sesame "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clj-sesame "0.1.0"
+  :description "A simple clojure wrapper for sesame"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
